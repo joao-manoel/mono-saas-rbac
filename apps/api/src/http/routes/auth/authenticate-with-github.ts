6854d585ee40b9e83abf91c6191f062c13057185
João Manoel <joaomanoel.ms.br@gmail.com>
2024-05-23 23:18:09 -0400

    feat: (created environment variables)

--- apps/api/src/http/routes/auth/authenticate-with-github.ts
+++ apps/api/src/http/routes/auth/authenticate-with-github.ts
@@ -1,3 +1,4 @@
+import { env } from '@saas/env'
 import { FastifyInstance } from 'fastify'
 import { ZodTypeProvider } from 'fastify-type-provider-zod'
 import z from 'zod'
@@ -29,14 +30,14 @@ export async function authenticateWithGithub(app: FastifyInstance) {
         'https://github.com/login/oauth/access_token',
       )
 
-      githubOAuthURL.searchParams.set('client_id', 'Ov23liK9uTytLjqLADW9')
+      githubOAuthURL.searchParams.set('client_id', env.GITHUB_OAUTH_CLIENT_ID)
       githubOAuthURL.searchParams.set(
         'client_secret',
-        'c2b26107ecf055ee001c470cf2395f24f4c5edf2',
+        env.GITHUB_OAUTH_CLIENT_SECRET,
       )
       githubOAuthURL.searchParams.set(
         'redirect_uri',
-        'http://localhost:3000/api/auth/callback',
+        env.GITHUB_OAUTH_CLIENT_REDIRECT_URI,
       )
       githubOAuthURL.searchParams.set('code', code)
 
